defe3e6e88ab3e1caa46050b8c2cf7d299294f56
O-Jun-S <jun.shima256@gmail.com>
2021-07-28 16:04:19 +0900

    Make foundation of hamburger button.

---
+++ js/hamburger.js
@@ -0,0 +1,13 @@
+window.onload = (function () {
+  $('#js-buttonHamburger').click(function () {
+    $('body').toggleClass('is-drawerActive');
+
+    if ($(this).attr('aria-expanded') == 'false') {
+      $(this).attr('aria-expanded', true);
+    }
+    
+    else {
+      $(this).attr('aria-expanded', false);
+    }
+  });
+}) ();
